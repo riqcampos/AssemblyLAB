4b7d129c08e1fe16eefa79506f911bc5047e9d79
Cesar <cesarhrccontato@gmail.com>
2024-04-08 16:20:09 -0300

    comentando o codigo de Lab02_3

--- Lab02/Lab02_3.asm
+++ Lab02/Lab02_3.asm
@@ -2,19 +2,19 @@ TITLE Inversor de vetor dentro de O(1)
 .model small
 .stack 100h
 .data
-    vetor DB 0,0,0,0,0,0,0
-    tamanho equ ($-vetor)
+    vetor DB 0,0,0,0,0,0,0 ;iniciando o vetor de 7 posicoes
+    tamanho equ ($-vetor) ;atribuindo o tamanho do vetor dentro de uma variavel
 .code
     main proc
         mov ax, @data
-        mov ds, ax
-        lea bx, vetor
-        mov cx, tamanho
-        call read_array
-        lea bx, vetor
-        lea si, vetor
-        add si, tamanho
-        call reversor
+        mov ds, ax ;permitindo o acesso as variaveis em .data
+        lea bx, vetor ;apontando para a primeira posicao do vetor
+        mov cx, tamanho ;iniciando o contador para o tamanho do vetor
+        call read_array ;chamando procedimento para ler os valores do array
+        lea bx, vetor ;pegando a primeira posicao do vetor 
+        lea si, vetor ;pegando a primeira posicao do vetor 
+        add si, tamanho ;levando o ponteiro ate a ultima posicao do vetor
+        call reversor ;chamando procedimento inversor
         lea bx, vetor
         mov cx, tamanho
         call print_array
@@ -25,24 +25,24 @@ TITLE Inversor de vetor dentro de O(1)
 ;procedimento para ler o vetor
 read_array proc
     READ:
-        mov ah, 01h
+        mov ah, 01h;funcao int 21 para ler o buffer
         int 21h
-        sub al, "0"
-        inc bx
-        mov vetor[bx], al
+        sub al, "0";transformando em numero
+        mov vetor[bx], al;atribindo o que foi lido a posicao do vetor
+        inc bx;incrementando a posicao do vetor 
         loop READ
         ret
 read_array endp
 ;procedimento para inverter o array
 reversor proc
     return:
-        mov al,vetor[bx]
-        xchg al,vetor[si]
-        mov vetor[bx],al
-        inc bx
-        dec si
-        cmp BX, Si
-        jle return
+        mov al,vetor[bx];jogando oque esta na posicao do vetor para um registrador auxiliar
+        xchg al,vetor[si];exchange entre posicao da ultima posicao do vetor e o que esta em al
+        mov vetor[bx],al; jogando oque esta em al para a posicao do vetor
+        inc bx;incrementa a posicao do vetor
+        dec si;decrementa o ponteiro da ultima posicao 
+        cmp bx, si; compara bx e si
+        jle return; volta se bx maior que si
         ret
 reversor endp
 ;procedimento para imprimir o array
@@ -50,11 +50,11 @@ print_array proc
 
     IMPRIME:
        
-        mov dl, vetor[bx]
-        or dl, 30h 
-        mov ah,02h
+        mov dl, vetor[bx]; movendo o que esta na posicao do vetor para dl
+        or dl, 30h ;transformando em caractere
+        mov ah,02h;funcao para printar
         int 21h
-        inc bx
+        inc bx;incrementando posicao do vetor
         loop IMPRIME
         ret
 print_array endp
